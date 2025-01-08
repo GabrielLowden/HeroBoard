095cae7611df1279c44e290db9ac605b4d8479dc
Gabriel Lowden <98785105+GabeLowden@users.noreply.github.com>
2025-01-08 15:03:57 -0500

    add a file

---
+++ LED Projects/PulseLED.ino
@@ -0,0 +1,14 @@
+int LEDPIN = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDPIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LEDPIN, HIGH);
+  delay(500);
+  digitalWrite(LEDPIN, LOW);
+  delay(500);
+}
